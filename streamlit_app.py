c48ee6969aa45c01bafadb0758198a1e916a55db
arodformacion <149065762+arodformacion@users.noreply.github.com>
2023-11-02 14:40:28 +0100

    Update streamlit_app.py
    Fixing control of flow

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,6 +33,9 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # show normalized result in table
 streamlit.dataframe(fruityvice_normalized)
 
+# don't run anything past here while we troubleshoot
+streamlit.stop()
+
 import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
