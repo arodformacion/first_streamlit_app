3eb2c45dca85f57ed07212c716e2bf0cbfb92405
arodformacion <149065762+arodformacion@users.noreply.github.com>
2023-11-03 10:12:59 +0100

    Update streamlit_app.py
    Encapsulated code in function

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,18 +23,23 @@ fruit_to_show = my_fruit_list.loc[fruit_selected]
 
 streamlit.dataframe(fruit_to_show)
 
+#create the repeatble code block (called a function)
+def get_fruityvice_data(this_fruit_choice)
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    return fruityvice_normalized
+
 #new section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 try:
- fruit_choice = streamlit.text_input('What fruit would you like information about?')
- if not fruit_choice:
-  streamlit.error("Please select a fruit to get information.")
- else:
-  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-  streamlit.dataframe(fruityvice_normalized)
+    fruit_choice = streamlit.text_input('What fruit would you like information about?')
+    if not fruit_choice:
+        streamlit.error("Please select a fruit to get information.")
+    else:
+        back_from_function = get_fruityvice_data(fruit_choice)
+        streamlit.dataframe(back_from_function)
 except URLError as e:
- streamlit.error()
+    streamlit.error()
 
 # don't run anything past here while we troubleshoot
 streamlit.stop()
